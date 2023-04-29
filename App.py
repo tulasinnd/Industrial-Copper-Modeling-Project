a2c2e950bb7ba8ac7c0e3ebe0a431b6f058cfaa2
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-04-30 02:19:53 +0530

    Add files via upload

---
+++ App.py
@@ -0,0 +1,116 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import LabelBinarizer
+import joblib
+
+import streamlit as st
+import re
+st.set_page_config(layout="wide")
+
+tab1, tab2 = st.tabs(["REGRESSION", "CLASSIFICATION"]) 
+with tab1:    
+        st.write( f'<h1 style="color:#009999;">PREDICT SELLING PRICE</h1>', unsafe_allow_html=True )
+
+        # Define the possible values for the dropdown menus
+        status_options = ['Won', 'Draft', 'To be approved', 'Lost', 'Not lost for AM', 'Wonderful', 'Revised', 'Offered', 'Offerable']
+        item_type_options = ['W', 'WI', 'S', 'Others', 'PL', 'IPL', 'SLAWR']
+        country_options = [28., 25., 30., 32., 38., 78., 27., 77., 113., 79., 26., 39., 40., 84., 80., 107., 89.]
+        application_options = [10., 41., 28., 59., 15., 4., 38., 56., 42., 26., 27., 19., 20., 66., 29., 22., 40., 25., 67., 79., 3., 99., 2., 5., 39., 69., 70., 65., 58., 68.]
+        product=['611112', '611728', '628112', '628117', '628377', '640400', '640405', '640665', 
+                     '611993', '929423819', '1282007633', '1332077137', '164141591', '164336407', 
+                     '164337175', '1665572032', '1665572374', '1665584320', '1665584642', '1665584662', 
+                     '1668701376', '1668701698', '1668701718', '1668701725', '1670798778', '1671863738', 
+                     '1671876026', '1690738206', '1690738219', '1693867550', '1693867563', '1721130331', '1722207579']
+
+        # Define the widgets for user input
+        with st.form("my_form"):
+            col1,col2,col3=st.columns([5,2,5])
+            with col1:
+                st.write(' ')
+                status = st.selectbox("Status", status_options,key=1)
+                item_type = st.selectbox("Item Type", item_type_options,key=2)
+                country = st.selectbox("Country", sorted(country_options),key=3)
+                application = st.selectbox("Application", sorted(application_options),key=4)                
+            with col3:
+                product_ref = st.selectbox("Product Reference", product,key=5)
+                st.write('NOTE: Below Range is given just for reference, you can enter any value')
+                quantity_tons = st.text_input("Enter Quantity Tons (Min:611728 & Max:1722207579)")
+                thickness = st.text_input("Enter thickness (Min:0.18 & Max:400)")
+                width = st.text_input("Enter width (Min:1, Max:2990)")
+                customer = st.text_input("customer ID (Min:12458, Max:30408185)")
+    
+            flag=0 
+            pattern = "^(?:\d+|\d*\.\d+)$"
+            for i in [quantity_tons,thickness,width,customer]:             
+                if re.match(pattern, i):
+                    pass
+                else:                    
+                    flag=1  
+                    break
+            submit_button = st.form_submit_button(label="Submit")
+        if submit_button and flag==1:
+            if len(i)==0:
+                st.write("please enter a valid number space not allowed")
+            else:
+                st.write("You have entered an invalid value: ",i)  
+             
+        if submit_button and flag==0:
+            # Print the user input
+            st.write("Status:", status)
+            st.write("Item Type:", item_type)
+            st.write("Country:", country)
+            st.write("Application:", application) 
+            st.write("Product Reference:", product_ref)
+            st.write("Quantity Tons:", quantity_tons)
+            st.write("Thickness:", thickness)
+            st.write("Width:", width)
+            st.write("Customer ID:", customer)
+
+            # loaded_model = joblib.load(r"C:\Users\91939\OneDrive\Desktop\Copper_P4\Decision_Tree_Regressor.joblib")
+
+            # # Load scaler object
+            # scaler = joblib.load(r"C:\Users\91939\OneDrive\Desktop\Copper_P4\scaler.joblib")
+
+            # # Load one-hot encoder object
+            # type_en = joblib.load(r"C:\Users\91939\OneDrive\Desktop\Copper_P4\type.joblib")
+
+            # # Load label binarizer object
+            # status_en = joblib.load(r"C:\Users\91939\OneDrive\Desktop\Copper_P4\status.joblib")
+
+            # ['quantity tons_log', 'application', 'thickness_log', 'width','country','customer','product_ref']].values, X_ohe, X_be
+            # new_sample = np.array([[np.log(700), 10, np.log(20), 10, 28,30202938,1670798778,'PL','Lost']])
+
+            import pickle
+            # Loading the model
+            with open(r"C:\Users\91939\OneDrive\Desktop\Copper_P4\model.pkl", 'rb') as file:
+                loaded_model = pickle.load(file)
+            # Load the objects using pickle
+            with open(r'C:\Users\91939\OneDrive\Desktop\Copper_P4\scaler.pkl', 'rb') as f:
+                scaler_loaded = pickle.load(f)
+
+            with open(r"C:\Users\91939\OneDrive\Desktop\Copper_P4\t.pkl", 'rb') as f:
+                t_loaded = pickle.load(f)
+
+            with open(r"C:\Users\91939\OneDrive\Desktop\Copper_P4\s.pkl", 'rb') as f:
+                s_loaded = pickle.load(f)
+
+            new_sample= np.array([[np.log(float(quantity_tons)),application,np.log(float(thickness)),float(width),country,float(customer),int(product_ref),item_type,status]])
+
+
+
+            #new_sample = np.array([[np.log(700), 10, np.log(20), 10, 28,30202938,1670798778,'PL','Won']])
+            new_sample_ohe = t_loaded.transform(new_sample[:, [7]]).toarray()
+            new_sample_be = s_loaded.transform(new_sample[:, [8]]).toarray()
+            new_sample = np.concatenate((new_sample[:, [0,1,2, 3, 4, 5, 6,]], new_sample_ohe, new_sample_be), axis=1)
+            new_sample1 = scaler_loaded.transform(new_sample)
+            new_pred = loaded_model.predict(new_sample)
+            print('Predicted selling price:', np.exp(new_pred))
+
+              
+with tab2:
+        st.write( f'<h1 style="color:#009999;">PREDICT STATUS</h1>', unsafe_allow_html=True )
