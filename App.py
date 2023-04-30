010908cc68b323089dfc4d86a13d7dd679cba4e6
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-04-30 12:42:30 +0530

    Update App.py

--- App.py
+++ App.py
@@ -107,7 +107,7 @@ with tab1:
             new_sample = np.concatenate((new_sample[:, [0,1,2, 3, 4, 5, 6,]], new_sample_ohe, new_sample_be), axis=1)
             new_sample1 = scaler_loaded.transform(new_sample)
             new_pred = loaded_model.predict(new_sample1)[0]
-            st.write('## Predicted selling price:', np.exp(new_pred))
+            st.write('## green[Predicted selling price:] ', np.exp(new_pred))
 
               
 with tab2:
