0f1536ffc83b369701b5e7e3ba2f6429d10879cd
TulasiNND <116662776+tulasinnd@users.noreply.github.com>
2023-04-30 14:23:26 +0530

    Update App.py

--- App.py
+++ App.py
@@ -152,5 +152,5 @@ with tab2:
             else:
                 st.write('## :red[The status is Lost] ')
                 
-st.write( f'<h6 style="color:rgb(0, 153, 153,0.2);">App Created by TulasiNND</h6>', unsafe_allow_html=True )  
+st.write( f'<h6 style="color:rgb(0, 153, 153,0.35);">App Created by TulasiNND</h6>', unsafe_allow_html=True )  
 
